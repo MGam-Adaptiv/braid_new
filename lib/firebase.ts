ffc9960b67896ed80c2f8c8df0f169794a0c50c0
Metaxas Gamvrelis <129938719+MGam-Adaptiv@users.noreply.github.com>
2026-03-02 19:17:41 +0200

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,7 +2,6 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -12,7 +11,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
 const app = firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
@@ -20,3 +18,6 @@ export const db = firebase.firestore();
 export const storage = firebase.storage();
 export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
 export const increment = firebase.firestore.FieldValue.increment;
+export const arrayUnion = firebase.firestore.FieldValue.arrayUnion;
+export const arrayRemove = firebase.firestore.FieldValue.arrayRemove;
+export const deleteField = firebase.firestore.FieldValue.delete;
